71f257ccaca620bccc03b4c4c10173d4358f1001
seohyeonKK <sts05158@gmail.com>
2021-09-25 02:02:26 +0900

    feat: 수정 버튼 생성 및 답변 수정 기능 구현

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,8 +1,11 @@
-import { Button } from 'reactstrap'
+import { Form, Input } from 'reactstrap'
 import 'css/Answer.css'
 import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Answer = (props) => {
+  const [edit, setEdit] = useState(false)
+  const [answerContents, setAnswerContents] = useState(props.answer)
   const checkQuestionNumber = (num) => {
     let questionNum = num.toString()
     if (questionNum.length < 2) {
@@ -11,21 +14,16 @@ const Answer = (props) => {
     return questionNum
   }
 
-  const likeIt = () => {
+  const likeAnswer = () => {
     axios({
       method: 'post',
       url: '/api/v1/answer/like',
-      // data: {
-      //   content: textContents,
-      //   bookmarkCount: 0,
-      //   tags: questionRegiTagArr,
-      // },
       params: {
         answerId: props.id,
       },
     })
       .then((res) => {
-        console.log(res)
+        // console.log(res)
         window.location.reload()
         window.alert('좋아요 되었습니다.')
       })
@@ -35,16 +33,76 @@ const Answer = (props) => {
       })
   }
 
+  useEffect(() => {
+    setAnswerContents(props.answer)
+  }, [edit])
+
+  const editAnswer = () => {
+    axios({
+      method: 'patch',
+      url: '/api/v1/answer/',
+      data: {
+        answerId: props.id,
+        content: answerContents,
+      },
+    })
+      .then((res) => {
+        window.location.reload()
+        window.alert('답변이 수정되었습니다.')
+        setEdit(false)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   return (
     <div className="each-answer">
       <div className="answer-card">
         <div className="answer-number">{checkQuestionNumber(props.number)}</div>
-        <div className="answer-content">
-          <h1>{props.title}</h1>
 
-          <h5>{props.answer}</h5>
-        </div>
-        <button onClick={likeIt}>좋아요! {props.like}</button>
+        {edit ? (
+          <>
+            <div className="answer-content">
+              <h1>{props.title}</h1>
+              <Form>
+                {/* <span id="text-counts">( {answerContents.length} / 1000 )</span> */}
+                {/* {console.log(answerContents)} */}
+                {/* 최대 길이 1000이 아니라 255? */}
+                <Input
+                  type="textarea"
+                  value={answerContents}
+                  maxLength="1000"
+                  onChange={(e) => {
+                    setAnswerContents(e.target.value)
+                  }}
+                  id="answer-contents"
+                />
+              </Form>
+            </div>
+            <button className="answer-like" onClick={likeAnswer}>
+              좋아요! {props.like}
+            </button>
+            <button className="answer-edit" onClick={editAnswer}>
+              수정완료
+            </button>
+          </>
+        ) : (
+          <>
+            <div className="answer-content">
+              <h1>{props.title}</h1>
+              <h5>{props.answer}</h5>
+            </div>
+            <button className="answer-like" onClick={likeAnswer}>
+              좋아요! {props.like}
+            </button>
+            {props.mine === props.id ? (
+              <button className="answer-edit" onClick={() => setEdit(true)}>
+                수정하기
+              </button>
+            ) : null}
+          </>
+        )}
       </div>
     </div>
   )
