f28d00fc2227d973ccc652422a4b9e894868c8df
seohyeonKK <sts05158@gmail.com>
2021-09-28 13:47:31 +0900

    답변의 길이 확인하는 함수 생성

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,3 +1,5 @@
+import { MIN_ANSWER_CONTENTS_LENGTH } from 'constants/constants'
+
 export const getZerofilledNumber = (num: number) => {
   let result = ''
   if (num < 0 && num < 10) result += '0'
@@ -6,4 +8,13 @@ export const getZerofilledNumber = (num: number) => {
 
 export const isNumeric = (str: string | null) => {
   return str !== null && !isNaN(Number(str))
+}
+
+export const checkAnswerLength = (answer: string) => {
+  const answerLength = answer.length
+  if (answerLength > 0 && answerLength < MIN_ANSWER_CONTENTS_LENGTH) {
+    return false
+  } else {
+    return true
+  }
 }
