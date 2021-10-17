81b8253f049645e2bf8d70f82db250a5c73d5ad2
seohyeonKK <sts05158@gmail.com>
2021-10-18 00:04:14 +0900

    username을 제외한 localstorage 삭제

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -45,7 +45,6 @@ const Navigation = (props: any) => {
           console.log(res.data)
           setUserProfile(res.data)
           localStorage.setItem('userName', res.data.username)
-          localStorage.setItem('userEmail', res.data.email)
         })
         .catch((err) => {
           console.log(err)
@@ -55,8 +54,6 @@ const Navigation = (props: any) => {
           setUserProfile(null)
           removeCookie('Authorization', { path: '/' })
           localStorage.removeItem('userName')
-          localStorage.removeItem('userEmail')
-          localStorage.removeItem('questionRegiTag')
         })
     }
   }, [])
@@ -141,8 +138,6 @@ const Navigation = (props: any) => {
                 onClick={() => {
                   removeCookie('Authorization', { path: '/' })
                   localStorage.removeItem('userName')
-                  localStorage.removeItem('userEmail')
-                  localStorage.removeItem('questionRegiTag')
                 }}>
                 로그아웃
               </a>
