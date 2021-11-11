dddbf40d6dedcdb38ee2102063967ff6ec51ed7a
seohyeonKK <sts05158@gmail.com>
2021-11-11 15:48:05 +0900

    로그인 모달 디자인 수정

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,44 +1,46 @@
 // todo: 타 tsx 파일과 코드 스타일 맞추기
 import React from 'react'
-import 'css/LoginModal.css'
+import styles from 'css/LoginModal.module.css'
 import { GOOGLE_AUTH_URL, GITHUB_AUTH_URL } from 'constants/Oauth'
 
-const googleLogo = '/img/google-logo.png'
-const githubLogo = '/img/github-logo.png'
+const iterviewLogo = '/img/iterview_logo_dark.png'
+const googleLogo = '/img/google_logo.png'
+const githubLogo = '/img/github_logo.png'
 
 function LoginModal(props: any) {
-  const { open, close, header } = props
+  const { open, close } = props
 
   return (
     // 모달이 열릴때 openModal 클래스가 생성
-    <div className={open ? 'openModal modal' : 'modal'}>
+    <>
       {open ? (
-        <section>
-          <header>
-            <img src="/img/LOGO3.png" alt="" />
-            <h1>통합 회원가입 및 로그인</h1>
-            <button className="close" onClick={close}>
+        <div className={styles.modal}>
+          <section>
+            <button className={styles.close} onClick={close}>
               &times;
             </button>
-          </header>
-          <main>
-            <a href={GOOGLE_AUTH_URL}>
-              <button className="google-btn">
-                <img id="google-logo" src={googleLogo} alt="googleLogo" />
-                Google으로 계속하기
-              </button>{' '}
-            </a>
-            <br />
-            <a href={GITHUB_AUTH_URL}>
-              <button className="github-btn">
-                <img id="github-logo" src={githubLogo} alt="githubLogo" />
-                Github으로 계속하기
-              </button>
-            </a>
-          </main>
-        </section>
+            <div className={styles.header}>
+              <img className={styles.iterviewLogo} src={iterviewLogo} alt="" />
+            </div>
+            <main>
+              <a href={GOOGLE_AUTH_URL}>
+                <button className={styles.googleLogin}>
+                  <img className={styles.googleLogo} src={googleLogo} alt="googleLogo" />
+                  <span>Google으로 계속하기</span>
+                </button>{' '}
+              </a>
+              <br />
+              <a href={GITHUB_AUTH_URL}>
+                <button className={styles.githubLogin}>
+                  <img className={styles.githubLogo} src={githubLogo} alt="githubLogo" />
+                  Github으로 계속하기
+                </button>
+              </a>
+            </main>
+          </section>
+        </div>
       ) : null}
-    </div>
+    </>
   )
 }
 
