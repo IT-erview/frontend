1afe94c343fcd2819f6a34a45099b7dad6742b94
seohyeonKK <sts05158@gmail.com>
2021-11-23 20:13:06 +0900

    비로그인시 상세페이지 접근 불가

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -3,20 +3,29 @@ import { Answer, Tag } from 'common/type'
 import { getZerofilledNumber } from 'common/util'
 import { MAX_DISPLAYED_TAG_COUNT } from 'common/config'
 import styles from 'css/Question.module.css'
+import { JWT_TOKEN } from 'constants/Oauth'
+import { useDispatch } from 'react-redux'
+import { setModalOpen } from 'modules/loginModal'
 
 const Question = (props: { id: number; number: number; content: string; tagList?: Array<Tag>; answer?: Answer }) => {
   // todo: 전체적으로 적용필요
   // todo: questionId 수정으로 이동 가능
   let isRequesting = false
+  const dispatch = useDispatch()
   const moveToQuestionDetail = async () => {
-    if (isRequesting) return
-    isRequesting = true
-    const myAnswer = await getMyAnswer(props.id).finally(() => (isRequesting = false))
-    if (!myAnswer) {
-      alert('문제에 대한 나의 답변이 없으면 다른 사람의 답변을 볼 수 없습니다. 답변 등록 페이지로 이동합니다.')
-      window.open(`/AnswerRegister?question_id=${props.id}`, '', '_blank')
+    if (!JWT_TOKEN) {
+      alert('로그인을 해주세요.')
+      dispatch(setModalOpen(true))
     } else {
-      window.open(`/QuestionDetail?question_id=${props.id}`)
+      if (isRequesting) return
+      isRequesting = true
+      const myAnswer = await getMyAnswer(props.id).finally(() => (isRequesting = false))
+      if (!myAnswer) {
+        alert('문제에 대한 나의 답변이 없으면 다른 사람의 답변을 볼 수 없습니다. 답변 등록 페이지로 이동합니다.')
+        window.open(`/AnswerRegister?question_id=${props.id}`, '', '_blank')
+      } else {
+        window.open(`/QuestionDetail?question_id=${props.id}`)
+      }
     }
   }
 
