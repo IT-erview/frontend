aa45ab74d260f389cc82ccd0f4d617dc198f2999
seohyeonKK <sts05158@gmail.com>
2022-02-24 19:35:49 +0900

    모달 디자인 수정

--- src/components/ExitAnswer.tsx
+++ src/components/ExitAnswer.tsx
@@ -11,7 +11,7 @@ function NextModal({ onClickToggleModal }: PropsWithChildren<ModalDefaultType>) 
     <div>
       <section className={styles.exitAnswerModal}>
         <img className={styles.logo} src="img/iterview_logo_dark.png" alt="logo" />
-        <h3>문제가 마음에 안드시나요?</h3>
+        <h3 className={styles.text}>답변 작성을 완료하셨나요?</h3>
         <div className={styles.buttonWrap}>
           <button className={`${styles.buttonBlue} ${styles.modalButton}`}>해당 종류 다른 문제 풀기</button>
           <Link to="/QuizResult" className={`${styles.buttonDarkBlue} ${styles.modalButton}`}>
