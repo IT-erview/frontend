b108afb51e5ecfe84feafec81cd8870325a8f70e
seohyeonKK <sts05158@gmail.com>
2022-02-24 19:37:09 +0900

    모달 디자인 수정 및 텍스트 수정

--- src/components/NextModal.tsx
+++ src/components/NextModal.tsx
@@ -21,7 +21,7 @@ function Modal({ onClickToggleNextModal }: PropsWithChildren<ModalDefaultType>) 
     <div>
       <section className={styles.exitAnswerModal}>
         <img className={styles.logo} src="img/iterview_logo_dark.png" alt="logo" />
-        <h3>답변 작성을 완료하셨나요?</h3>
+        <h3 className={styles.text}>답변 작성을 완료하셨나요?</h3>
         <div className={styles.buttonWrap}>
           <button onClick={deepQuestion} className={`${styles.buttonBlue} ${styles.modalButton}`}>
             해당 문제 심화 문제 풀기
@@ -31,7 +31,7 @@ function Modal({ onClickToggleNextModal }: PropsWithChildren<ModalDefaultType>) 
           </button>
         </div>
         <div className={styles.buttonExitWrap}>
-          <button onClick={onClickToggleNextModal}>답안 작성을 돌아가기</button>
+          <button onClick={onClickToggleNextModal}>답안 작성으로 돌아가기</button>
         </div>
       </section>
     </div>
