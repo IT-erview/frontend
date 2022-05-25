c264ed71e67a6853535ee0f45691c0f29dce5c14
seohyeonKK <sts05158@gmail.com>
2022-05-25 14:31:38 +0900

    검색 컴포넌트 분리

---
+++ src/views/common/tag/TagSearch.tsx
@@ -0,0 +1,116 @@
+import { ReducerType } from 'modules/rootReducer'
+import { setSearchTagSelected } from 'modules/searchTags'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory, useLocation } from 'react-router-dom'
+import { MAX_SEARCH_TAG_LENGTH } from 'utils/config'
+import { TagSelectorItem } from 'utils/type'
+
+const TagSearch = () => {
+  const [text, setText] = useState('')
+  const location = useLocation()
+  const history = useHistory()
+  const searchTags = useSelector<ReducerType, Array<TagSelectorItem>>((state) => state.searchTags)
+  const [searchingTags, setSearchingTags] = useState<Array<TagSelectorItem>>([])
+  const [selectedTags, setSelectedTags] = useState<Array<TagSelectorItem>>(
+    searchTags.filter((tag) => tag.isSelected === true),
+  )
+  const dispatch = useDispatch()
+  const onTagSelect = (tagId: number, isSelected: boolean) => dispatch(setSearchTagSelected({ tagId, isSelected }))
+
+  const selectTag = (tag: TagSelectorItem) => {
+    onTagSelect(tag.id, !tag.isSelected)
+    setText('')
+    if (tag.isSelected) {
+      setSelectedTags(selectedTags.filter((item) => item.id !== tag.id))
+    } else {
+      setSelectedTags([...selectedTags, { id: tag.id, name: tag.name, isSelected: !tag.isSelected }])
+    }
+  }
+
+  const findTags = (tags: Array<TagSelectorItem>, text: string) => {
+    const result = [] as Array<TagSelectorItem>
+    tags.forEach((tag) => {
+      const tagNameLower = tag.name.toLowerCase()
+      const textLower = text.toLowerCase()
+      if (tagNameLower.indexOf(textLower) !== -1) result.push(tag)
+    })
+    if (result.length === 0) return tags
+    else return result
+  }
+
+  const resetSelectedTags = () => {
+    selectedTags.forEach((tag) => {
+      onTagSelect(tag.id, !tag.isSelected)
+    })
+    setSelectedTags([])
+  }
+
+  const searchQuestions = () => {
+    if (location.pathname !== '/QuestionSearch') {
+      history.push('/QuestionSearch')
+    }
+  }
+
+  const showTags = searchingTags.map((tag, index) => {
+    return (
+      <li key={index}>
+        <button onClick={() => selectTag(tag)} className={`${tag.isSelected ? 'selected' : ''} tag`}>
+          {tag.name}
+        </button>
+      </li>
+    )
+  })
+
+  const showSelectedTags = selectedTags.map((tag, index) => {
+    return (
+      <li key={index}>
+        <button className={'selected-tag'} onClick={() => selectTag(tag)}>
+          {tag.name} X
+        </button>
+      </li>
+    )
+  })
+
+  useEffect(() => {
+    setSearchingTags(findTags(searchTags, text))
+  }, [searchTags, text])
+
+  return (
+    <article className={'search-wrap'}>
+      <div className={'search-box'}>
+        <div className={'search-input-wrap'}>
+          <input
+            type="text"
+            maxLength={MAX_SEARCH_TAG_LENGTH}
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value)
+            }}
+            placeholder="검색어를 입력해주세요."
+            className={'search-input'}
+          />
+          <button onClick={searchQuestions} className={'search-button'}>
+            검색하기
+          </button>
+        </div>
+        <p className={'search-information'}>
+          일일이 찾아야 했던 면접 질문과 답변들, 검증되지 않았던 정보들, 한 번에 검색하고 검증된 정보를 받아보세요.
+        </p>
+      </div>
+      {selectedTags.length > 0 && (
+        <div className={'selected-tag-wrap'}>
+          <ul>{showSelectedTags}</ul>
+          <button onClick={resetSelectedTags} className={'btn-reset'}>
+            검색어 초기화 X
+          </button>
+        </div>
+      )}
+      <div className={'tag-wrap'}>
+        <ul>{showTags}</ul>
+      </div>
+    </article>
+  )
+}
+
+export default TagSearch
--- src/views/main/page/MainPage.tsx
+++ src/views/main/page/MainPage.tsx
@@ -1,17 +1,13 @@
 // memo 제외하고 대부분 완료
-// import 'css/MainPage.css'
 // react
 import { useCallback, useEffect, useState } from 'react'
-import { useHistory } from 'react-router'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { ReducerType } from 'modules/rootReducer'
 // util
-import { MAX_SEARCH_TAG_LENGTH } from 'utils/config'
-import { Answer as AnswerType, Question as QuestionType, TagSelectorItem } from 'utils/type'
+import { Answer as AnswerType, Question as QuestionType } from 'utils/type'
 // style
 import 'views/main/style/MainPage.sass'
 // redux
-import { setSearchTagSelected } from 'modules/searchTags'
 // api
 import { getHitsAnswers } from 'api/answer'
 import { getHitsQuestions } from 'api/question'
@@ -21,91 +17,16 @@ import Footer from 'views/common/layout/Footer'
 import LoginModal from 'views/common/login/LoginModal'
 import Question from 'views/common/question/Question'
 import Answer from 'views/common/answer/Answer'
-
-// enum FeatureDescriptionType {
-//   SEARCH,
-//   REGISTER,
-//   MY_PAGE,
-//   QUIZ,
-// }
-// memo: 쓰임새 보고 global로 변경
-// const isAuthorized = () => {
-//   return getCookie('Authorization')
-// }
-
-// const featureDescriptions = [
-//   {
-//     title: '문제 검색',
-//     description: '문제 검색 기능을 통해 태그와 키워드로 원하는 문제를 검색해보세요!',
-//     type: FeatureDescriptionType.SEARCH,
-//   },
-//   {
-//     title: '문제 등록',
-//     description: '문제 등록 기능을 통해 궁금했던 문제를 등록하여 다른사람의 답변을 받아보세요!',
-//     type: FeatureDescriptionType.REGISTER,
-//   },
-//   {
-//     title: '마이페이지',
-//     description: "마이페이지 기능을 통해 나의 IT'erview 기록들을 확인해보세요!",
-//     type: FeatureDescriptionType.MY_PAGE,
-//   },
-//   {
-//     title: '퀴즈',
-//     description: '퀴즈 기능을 통해 모의 면접을 경험해보세요!',
-//     type: FeatureDescriptionType.QUIZ,
-//   },
-// ]
+import TagSearch from 'views/common/tag/TagSearch'
 
 const MainPage = () => {
-  const dispatch = useDispatch()
   const loginModal = useSelector<ReducerType, boolean>((state) => state.loginModal)
-  const searchTags = useSelector<ReducerType, Array<TagSelectorItem>>((state) => state.searchTags)
-  const [tagSearchText, setTagSearchText] = useState<string>('')
-  const [searchingTags, setSearchingTags] = useState<Array<TagSelectorItem>>([])
   const [hitsQuestions, setHitQuestions] = useState<Array<QuestionType>>([])
   const [hitsAnswers, setHitAnswers] = useState<Array<AnswerType>>([])
-  const history = useHistory()
   const [questionSort, setQuestionSort] = useState<string>('weekly')
   const [answerSort, setAnswerSort] = useState<string>('weekly')
   const [moreQuestion, setMoreQuestion] = useState<boolean>(false)
   const [moreAnswer, setMoreAnswer] = useState<boolean>(false)
-  const [selectedTags, setSelectedTags] = useState<Array<TagSelectorItem>>(
-    searchTags.filter((tag) => tag.isSelected === true),
-  )
-
-  const onTagSelect = (tagId: number, isSelected: boolean) => dispatch(setSearchTagSelected({ tagId, isSelected }))
-
-  const selectTag = (tag: TagSelectorItem) => {
-    onTagSelect(tag.id, !tag.isSelected)
-    setTagSearchText('')
-    if (tag.isSelected) {
-      setSelectedTags(selectedTags.filter((item) => item.id !== tag.id))
-    } else {
-      setSelectedTags([...selectedTags, { id: tag.id, name: tag.name, isSelected: !tag.isSelected }])
-    }
-  }
-
-  const findTags = (tags: Array<TagSelectorItem>, text: string) => {
-    const result = [] as Array<TagSelectorItem>
-    tags.forEach((tag) => {
-      const tagNameLower = tag.name.toLowerCase()
-      const textLower = text.toLowerCase()
-      if (tagNameLower.indexOf(textLower) !== -1) result.push(tag)
-    })
-    if (result.length === 0) return tags
-    else return result
-  }
-
-  const resetSelectedTags = () => {
-    selectedTags.forEach((tag) => {
-      onTagSelect(tag.id, !tag.isSelected)
-    })
-    setSelectedTags([])
-  }
-
-  useEffect(() => {
-    setSearchingTags(findTags(searchTags, tagSearchText))
-  }, [searchTags, tagSearchText])
 
   const getQuestions = useCallback(async () => {
     let params = {
@@ -129,26 +50,6 @@ const MainPage = () => {
   useEffect(() => {
     getAnswers()
   }, [getAnswers])
-
-  const showTags = searchingTags.map((tag, index) => {
-    return (
-      <li key={index}>
-        <button onClick={() => selectTag(tag)} className={`${tag.isSelected ? 'selected' : ''} tag`}>
-          {tag.name}
-        </button>
-      </li>
-    )
-  })
-
-  const showSelectedTags = selectedTags.map((tag, index) => {
-    return (
-      <li key={index}>
-        <button className={'selected-tag'} onClick={() => selectTag(tag)}>
-          {tag.name} X
-        </button>
-      </li>
-    )
-  })
 
   const moreHideBtn = (hitsLength: number, more: boolean) => {
     if (hitsLength > 3) {
@@ -309,46 +210,7 @@ const MainPage = () => {
                 </p>
               </div>
             </article>
-            <article className={'search-wrap'}>
-              <h2 className={'search-title'}>문제 검색</h2>
-              <div className={'search-box'}>
-                <div className={'search-input-wrap'}>
-                  <input
-                    type="text"
-                    maxLength={MAX_SEARCH_TAG_LENGTH}
-                    value={tagSearchText}
-                    onChange={(e) => {
-                      setTagSearchText(e.target.value)
-                    }}
-                    placeholder="검색어를 입력해주세요."
-                    className={'search-input'}
-                  />
-                  <button
-                    onClick={() => {
-                      history.push('/QuestionSearch')
-                    }}
-                    className={'search-button'}>
-                    검색하기
-                  </button>
-                </div>
-                <p className={'search-information'}>
-                  일일이 찾아야 했던 면접 질문과 답변들, 검증되지 않았던 정보들, 한 번에 검색하고 검증된 정보를
-                  받아보세요.
-                </p>
-              </div>
-
-              {selectedTags.length > 0 && (
-                <div className={'selected-tag-wrap'}>
-                  <ul>{showSelectedTags}</ul>
-                  <button onClick={resetSelectedTags} className={'btn-reset'}>
-                    검색어 초기화 X
-                  </button>
-                </div>
-              )}
-              <div className={'tag-wrap'}>
-                <ul>{showTags}</ul>
-              </div>
-            </article>
+            <TagSearch />
           </div>
         </section>
         <section className="content-wrap">
