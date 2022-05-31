904175ddaa92d08035e906cb77eeaa3cfab7cecf
chdaud78 <qkrri56@naver.com>
2022-05-31 09:31:02 +0900

    [ENH] navigation style 수정

--- src/views/common/layout/Navigation.tsx
+++ src/views/common/layout/Navigation.tsx
@@ -139,119 +139,121 @@ const Navigation = () => {
 
   return (
     <nav className={'navigation'}>
-      <div className={'logo-wrap'}>
-        <Link to="/">
-          <img src="/img/iterview_logo_dark.png" alt="logo" />
-        </Link>
-      </div>
-      <div className={'menu-wrap'}>
-        <ul>
-          <li>
-            <Link to="/">
-              <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M0.699951 7.50011H13.3M1.04995 5.40012H12.95M2.09995 3.30012H11.9M1.04995 9.60011H12.95M2.09995 11.7001H11.9M6.99995 1.0188V13.9057M5.24995 1.36996C2.79995 5.56996 2.79995 9.43098 5.24995 13.631M8.74995 1.30896C11.55 5.50896 11.9 9.43098 8.74995 13.631"
-                  stroke="#09C558"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <circle cx="7" cy="7.5" r="6.5" stroke="#09C558" />
-              </svg>
-              <span>Home</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/MyPage">
-              <svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M3.375 8.375V14.5H15.075V8.375M1.125 7.9375L9.675 0.9375L16.875 7.9375M3.375 4.4375V0.5H5.175V2.6875"
-                  stroke="#160CFB"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <span>마이페이지</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/QuestionSearch">
-              <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="5.44444" cy="5.94444" r="4.94444" stroke="#FC2F31" />
-                <path
-                  d="M9.33325 9.44434L13.9999 14.4999"
-                  stroke="#FC2F31"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <span>문제검색</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/QuestionRegister">
-              <svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M3.14555 2.95324H14.8546M3.14555 5.89713H14.8546M3.14555 8.35038H10.2728M3.14555 11.2943H0.600098V0.5H17.4001V11.2943H9.25464L3.14555 14.5V11.2943Z"
-                  stroke="#D58A34"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <span>문제등록</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/SetQuizOptions" onClick={quizReset}>
-              <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="8" cy="7.5" r="6.5" stroke="#FAE45B" />
-                <path
-                  d="M5.2778 7.50003H8.00003V4.00003M1.38892 7.50003H2.55558M14.6111 7.50003H13.4445M8.00003 0.888916V2.05558M8.00003 14.1111V12.9445"
-                  stroke="#FAE45B"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <circle cx="7.99995" cy="7.49995" r="0.777778" fill="#FAE45B" />
-              </svg>
-              <span>퀴즈</span>
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="login-wrap">
-        {userName ? (
-          <>
-            <div className={'login-button-wrap'} onClick={toggle}>
-              <img src={userImgUrl} alt="user-profile-img" className={'user-profile-image'} />
-              <span>{userName} 님</span>
-              <div className={'dropdown-button-wrap'}>
-                <img src="/img/nav_icon7.png" className={isOpen ? 'arrow-up' : 'arrow-down'} alt="login_icon" />
+      <div className="navigation-wrap">
+        <div className={'logo-wrap'}>
+          <Link to="/">
+            <img src="/img/iterview_logo_dark.png" alt="logo" />
+          </Link>
+        </div>
+        <div className={'menu-wrap'}>
+          <ul>
+            <li>
+              <Link to="/">
+                <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M0.699951 7.50011H13.3M1.04995 5.40012H12.95M2.09995 3.30012H11.9M1.04995 9.60011H12.95M2.09995 11.7001H11.9M6.99995 1.0188V13.9057M5.24995 1.36996C2.79995 5.56996 2.79995 9.43098 5.24995 13.631M8.74995 1.30896C11.55 5.50896 11.9 9.43098 8.74995 13.631"
+                    stroke="#09C558"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <circle cx="7" cy="7.5" r="6.5" stroke="#09C558" />
+                </svg>
+                <span>Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/MyPage">
+                <svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M3.375 8.375V14.5H15.075V8.375M1.125 7.9375L9.675 0.9375L16.875 7.9375M3.375 4.4375V0.5H5.175V2.6875"
+                    stroke="#160CFB"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                <span>마이페이지</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/QuestionSearch">
+                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="5.44444" cy="5.94444" r="4.94444" stroke="#FC2F31" />
+                  <path
+                    d="M9.33325 9.44434L13.9999 14.4999"
+                    stroke="#FC2F31"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                <span>문제검색</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/QuestionRegister">
+                <svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M3.14555 2.95324H14.8546M3.14555 5.89713H14.8546M3.14555 8.35038H10.2728M3.14555 11.2943H0.600098V0.5H17.4001V11.2943H9.25464L3.14555 14.5V11.2943Z"
+                    stroke="#D58A34"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                <span>문제등록</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/SetQuizOptions" onClick={quizReset}>
+                <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="8" cy="7.5" r="6.5" stroke="#FAE45B" />
+                  <path
+                    d="M5.2778 7.50003H8.00003V4.00003M1.38892 7.50003H2.55558M14.6111 7.50003H13.4445M8.00003 0.888916V2.05558M8.00003 14.1111V12.9445"
+                    stroke="#FAE45B"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <circle cx="7.99995" cy="7.49995" r="0.777778" fill="#FAE45B" />
+                </svg>
+                <span>퀴즈</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="login-wrap">
+          {userName ? (
+            <>
+              <div className={'login-button-wrap'} onClick={toggle}>
+                <img src={userImgUrl} alt="user-profile-img" className={'user-profile-image'} />
+                <span>{userName} 님</span>
+                <div className={'dropdown-button-wrap'}>
+                  <img src="/img/nav_icon7.png" className={isOpen ? 'arrow-up' : 'arrow-down'} alt="login_icon" />
+                </div>
               </div>
-            </div>
-            {isOpen ? dropdownMenu() : ''}
-          </>
-        ) : (
-          <>
-            <div className={'login-button-wrap'} onClick={openModal}>
-              <svg width="18" height="18" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="7.5" cy="7.5" r="7" stroke="#111036" />
-                <path
-                  d="M10.3334 5.00008C10.3334 6.56489 9.06489 7.83342 7.50008 7.83342C5.93527 7.83342 4.66675 6.56489 4.66675 5.00008C4.66675 3.43527 5.93527 2.16675 7.50008 2.16675C9.06489 2.16675 10.3334 3.43527 10.3334 5.00008Z"
-                  stroke="#111036"
-                  strokeLinecap="round"
-                />
-                <path
-                  d="M3.33325 12.9167C4.07399 9.58341 4.99992 9.16675 7.49992 9.16675C9.99992 9.16675 10.9258 9.58341 11.6666 12.9167"
-                  stroke="#111036"
-                  strokeLinecap="round"
-                />
-              </svg>
-              <span>LOGIN</span>
-              <div className={'dropdown-button-wrap'}>
-                <img src="/img/nav_icon7.png" className={'dropdown-button'} alt="login_icon" />
+              {isOpen ? dropdownMenu() : ''}
+            </>
+          ) : (
+            <>
+              <div className={'login-button-wrap'} onClick={openModal}>
+                <svg width="18" height="18" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="7.5" cy="7.5" r="7" stroke="#111036" />
+                  <path
+                    d="M10.3334 5.00008C10.3334 6.56489 9.06489 7.83342 7.50008 7.83342C5.93527 7.83342 4.66675 6.56489 4.66675 5.00008C4.66675 3.43527 5.93527 2.16675 7.50008 2.16675C9.06489 2.16675 10.3334 3.43527 10.3334 5.00008Z"
+                    stroke="#111036"
+                    strokeLinecap="round"
+                  />
+                  <path
+                    d="M3.33325 12.9167C4.07399 9.58341 4.99992 9.16675 7.49992 9.16675C9.99992 9.16675 10.9258 9.58341 11.6666 12.9167"
+                    stroke="#111036"
+                    strokeLinecap="round"
+                  />
+                </svg>
+                <span>LOGIN</span>
+                <div className={'dropdown-button-wrap'}>
+                  <img src="/img/nav_icon7.png" className={'dropdown-button'} alt="login_icon" />
+                </div>
               </div>
-            </div>
-          </>
-        )}
+            </>
+          )}
+        </div>
       </div>
     </nav>
   )
