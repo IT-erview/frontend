a2f80b63f28b5e7a2a49c823978be25faca485ae
cherry2030 <64332359+cherry2030@users.noreply.github.com>
2022-10-19 18:18:44 +0900

    [ENH] 좋아요 기능 추가

--- src/views/common/answer/Answer.tsx
+++ src/views/common/answer/Answer.tsx
@@ -1,5 +1,6 @@
 // react
-//oauth
+// oauth
+import { JWT_TOKEN } from 'constants/Oauth'
 // util
 import { getZerofilledNumber } from 'utils/util'
 import { Tag } from 'utils/type'
@@ -8,7 +9,31 @@ import { MAX_DISPLAYED_TAG_COUNT } from 'utils/config'
 import 'views/common/answer/Answer.sass'
 // redux
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 // api
+import { likeAnswer } from 'api/answer'
+
+const alarmLike = (
+  id: number,
+  e: React.MouseEvent<HTMLElement>,
+  isLiked: boolean,
+  setIsLiked: React.Dispatch<React.SetStateAction<boolean>>,
+  setLikeCount: React.Dispatch<React.SetStateAction<number>>,
+) => {
+  e.preventDefault()
+  if (isLiked === false) {
+    setIsLiked(true)
+    setLikeCount((current) => current + 1)
+    likeAnswer(id)
+    alert('좋아요 되었습니다!')
+  }
+  // else{
+  //   setIsLiked(false)
+  //   setLikeCount((current) => current - 1)
+  //   likeAnswer(id)
+  //   alert('좋아요 되었습니다!')
+  // }
+}
 
 const Answer = (props: {
   id: number
@@ -19,6 +44,10 @@ const Answer = (props: {
   like: boolean
   tagList?: Array<Tag>
 }) => {
+  const [isLiked, setIsLiked] = useState(props.like)
+  console.log(`${props.id}: ${props.like}`)
+  const [likeCount, setLikeCount] = useState(props.likeCount)
+
   return (
     <Link to={`/QuestionDetail/${props.id}`}>
       <div className={'answer-box'}>
@@ -26,14 +55,15 @@ const Answer = (props: {
           <div className={'answer-index-wrap'}>
             <span className={'answer-index'}>{getZerofilledNumber(props.number)}</span>
           </div>
-          <div className={'answer-like-wrap'}>
-            {/* <button onClick={like}>좋아요! {props.like}</button> */}
+          <div
+            className={'answer-like-wrap'}
+            onClick={JWT_TOKEN ? (e) => alarmLike(props.id, e, isLiked, setIsLiked, setLikeCount) : undefined}>
             <img
-              src={props.like ? 'img/like_true.png' : 'img/like_false.png'}
+              src={isLiked ? 'img/like_true.png' : 'img/like_false.png'}
               className={'answer-like-icon'}
               alt="answerLike"
             />
-            <p className={'answer-like-count'}>{props.likeCount}</p>
+            <p className={'answer-like-count'}>{likeCount}</p>
           </div>
         </div>
         <div className={'answer-content-wrap'}>
